67e2e8949c588b88668bfb8c7211fee41ed6b525
Thushara Thiwanka <thusharathiwanka.dev@gmail.com>
2022-09-08 12:20:44 +0530

    CREATE: usequery created for treatments, staff fetching

--- base-lazy-days/client/src/components/staff/hooks/useStaff.ts
+++ base-lazy-days/client/src/components/staff/hooks/useStaff.ts
@@ -2,14 +2,15 @@ import { Dispatch, SetStateAction, useState } from 'react';
 
 import type { Staff } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
-import { queryKeys } from '../../../react-query/constants';
 import { filterByTreatment } from '../utils';
+import { queryKeys } from '../../../react-query/constants';
+import { useQuery } from 'react-query';
 
 // for when we need a query function for useQuery
-// async function getStaff(): Promise<Staff[]> {
-//   const { data } = await axiosInstance.get('/staff');
-//   return data;
-// }
+async function getStaff(): Promise<Staff[]> {
+  const { data } = await axiosInstance.get('/staff');
+  return data;
+}
 
 interface UseStaff {
   staff: Staff[];
@@ -21,8 +22,9 @@ export function useStaff(): UseStaff {
   // for filtering staff by treatment
   const [filter, setFilter] = useState('all');
 
+  const fallback = [];
   // TODO: get data from server via useQuery
-  const staff = [];
+  const { data: staff = fallback } = useQuery(queryKeys.staff, getStaff);
 
   return { staff, filter, setFilter };
 }
