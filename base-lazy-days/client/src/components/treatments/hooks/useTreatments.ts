427ab531ec4d649cc6b223f4619bd96412bc7dd2
Thushara Thiwanka <thusharathiwanka.dev@gmail.com>
2022-09-08 13:14:18 +0530

    CREATE: prefetching treatments added

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -1,7 +1,8 @@
+import { useQuery, useQueryClient } from 'react-query';
+
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
-import { useQuery } from 'react-query';
 
 // for when we need a query function for useQuery
 async function getTreatments(): Promise<Treatment[]> {
@@ -15,4 +16,9 @@ export function useTreatments(): Treatment[] {
   const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
 
   return data;
+}
+
+export function usePrefetchTreatments(): void {
+  const queryClient = useQueryClient();
+  queryClient.prefetchQuery(queryKeys.treatments, getTreatments);
 }
